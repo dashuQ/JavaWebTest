43526290ccc6204e6a8f63dc55a1086a37610b27
dashuQ <121965765@qq.com>
2018-03-01 11:40:14 +0800

    init

---
+++ src/com/hsg/bfc/test/Test.java
@@ -0,0 +1,12 @@
+package com.hsg.bfc.test;
+
+import com.google.gson.Gson;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java Web");
+        Gson gson=new Gson();
+    }
+
+}
